79896dd3e4b7015f1e8148b26ff634581d81343f
kariwah~ <karinav.romero2603@gmail.com>
2022-04-03 15:57:47 -0400

    quinto commit, se añade footer y se modifica script para que funcione toolstip y popover

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* tooltips bootstrap */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+/* popovers bootstrap */
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+/*mi funcion js */
+$(function () {
+    $('#enviar').click(function(){
+        alert("Ha sido enviado a su correo...")
+    })
+})
